5231edafa2dc352b33a7ee99ae85db999ab6a601
Antônio Santana <antonionetosg6@gmail.com>
2022-09-20 08:03:21 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente FavoritesPokemons', () => {
+  it('Teste se é exibida a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const messageNotFound = screen.getByText(/no favorite pokemon found/i);
+    expect(messageNotFound).toBeInTheDocument();
+  });
+});
