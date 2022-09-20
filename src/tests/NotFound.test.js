82fff4bda137da8fb89369e052dd5500065fba61
Antônio Santana <antonionetosg6@gmail.com>
2022-09-20 08:08:33 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound.js', () => {
+  it('Teste se a página contém um heading com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole(
+      'heading',
+      { name: /page requested not found/i, level: 2 },
+    );
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a url de imagem na página', () => {
+    renderWithRouter(<NotFound />);
+
+    const screenImage = screen.getByRole('img');
+    expect(screenImage).toBeInTheDocument();
+    expect(screenImage).toHaveAttribute(
+      'src',
+      expect.stringContaining(
+        'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+      ),
+    );
+  });
+});
