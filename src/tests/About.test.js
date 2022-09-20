20bdc3f23706dbb33d600cf852d4d6af8110645b
Antônio Santana <antonionetosg6@gmail.com>
2022-09-20 07:56:26 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,26 @@
-test('', () => {});
+import React from "react";
+import { About } from "../pages";
+import renderWithRouter from "../renderWithRouter";
+import { screen } from '@testing-library/react';
 
-// Iniciando o projeto
+describe('Testa o componente About.js', () => {
+
+    it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+        renderWithRouter(<About />);
+
+        const heading = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
+        expect(heading).toBeInTheDocument();
+    });
+
+    it('Teste se a página contém a url de imagem na pokédex', () => {
+        renderWithRouter(<About />);
+
+        const screenImage = screen.getByRole('img', { name: /pokédex/i });
+        expect(screenImage).toBeInTheDocument();
+        expect(screenImage).toHaveAttribute('src',
+            expect.stringContaining(
+                'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+            ));
+    });
+
+})
