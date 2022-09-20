5b5d0f6165cf6bd6093cedc5809e10cc5299da08
Antônio Santana <antonionetosg6@gmail.com>
2022-09-20 07:47:49 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+
+    it('Teste se a aplicação possui um link com o nome "Home"', () => {
+        renderWithRouter(<App />);
+
+        const linkHome = screen.getByRole('link', { name: /home/i });
+        expect(linkHome).toBeInTheDocument;
+    });
+
+    it('Teste se a aplicação possui um link com o nome "About"', () => {
+        renderWithRouter(<App />);
+
+        const linkAbout = screen.getByRole('link', { name: /about/i });
+        expect(linkAbout).toBeInTheDocument();
+    });
+
+    it('Teste se a aplicação possui um link com o nome "Favorites Pokémons"', () => {
+        renderWithRouter(<App />);
+
+        const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+        expect(linkFavorite).toBeInTheDocument();
+    });
+});
